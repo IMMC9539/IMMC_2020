725aa6ac3fc1e49c43ebc476d9e648955d8ce3d7
IMMC9539 <64113992+IMMC9539@users.noreply.github.com>
2020-04-25 10:35:08 -0400

    Update calculate_score.py

--- calculate_score.py
+++ calculate_score.py
@@ -7,7 +7,6 @@ This program calculates the damage score.
 ###########
 # IMPORTS #
 ###########
-
 import information as i
 from Department import Department
 import utils
@@ -18,22 +17,22 @@ def get_score():
   scores = []
 
   for dep in i.departments:
-    print("DEPARTMENT: " + str(dep.get_dept()))
+    # print("DEPARTMENT: " + str(dep.get_dept()))
 
     dept_score = total_score(dep)
     scores.append((dept_score/3) * i.max_damages)
 
-    print("TOTAL SCORE (0-3): " + str(dept_score))
-    print("PERCENT PRODUCTS DAMAGED: " + str((dept_score/3)*i.max_damages))
-    print("\n\n")
+    # print("TOTAL SCORE (0-3): " + str(dept_score))
+    # print("PERCENT PRODUCTS DAMAGED: " + str((dept_score/3)*i.max_damages))
+    # print("\n\n")
 
   sum_damaged = 0
   for j, score in enumerate(scores):
     products_damaged = i.departments[j].get_quantity() * score
     sum_damaged += products_damaged
 
-    print("DEPARTMENT: " + str(dep.get_dept()))
-    print("PRODUCTS DAMAGED: " + str(products_damaged) + "\n")
+    # print("DEPARTMENT: " + str(dep.get_dept()))
+    # print("PRODUCTS DAMAGED: " + str(products_damaged) + "\n")
 
   print("TOTAL PRODUCTS DAMAGED: " + str(sum_damaged))
 
@@ -44,10 +43,11 @@ def total_score(dep):
     rough_handling = dep.rough_handling()
     open_packages = dep.open_packaging()
 
-    print("--------------------------")
-    print("DROPPING SCORE:: ", dropping)
-    print("ROUGH HANDLING SCORE:: ", rough_handling)
-    print("OPENING PACKAGING SCORE:: ", open_packages)
-    print("--------------------------")
+    # print("--------------------------")
+    # print("DROPPING SCORE:: ", dropping)
+    # print("ROUGH HANDLING SCORE:: ", rough_handling)
+    # print("OPENING PACKAGING SCORE:: ", open_packages)
+    # print("--------------------------")
 
+    # damage score
     return i.w_1_T * dropping + i.w_2_T * rough_handling + i.w_3_T * open_packages
