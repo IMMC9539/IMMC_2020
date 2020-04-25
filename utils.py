ed33167d9399cfa0d3ee2aaaa8f08533021f56dd
IMMC9539 <64113992+IMMC9539@users.noreply.github.com>
2020-04-25 10:37:22 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -19,13 +19,32 @@ def population_density(num_people, pop_perc, area_of_department):
 def product_density(num_products, display_area):
     return num_products / display_area
 
-def visibility(cashier_pos, dep_pos, pop_per, max_dist = (2*(48**2))**0.5):
+def obstruction(cashier_pos, dep_pos, pop_per, max_dist = (2*(480**2))**0.5):
     '''
     A Gaussian function with average change in decrease as a function of the popularity percentage.
     '''
-    dist = ((max_dist - cashier_pos.distance(dep_pos))/(max_dist))
 
-    return np.e ** (-1 * (1 - pop_per) * (dist ** 2))
+    dist = ((cashier_pos.distance(dep_pos))/(max_dist))
+    # return np.e ** (-1 * (pop_per) * (dist ** 2))
+    return dist
+    
+
+def model_obstructions(obstructions, sub_constant = 0.1):
+  '''
+  Calculates the final obstruction score based on the
+  individual obstruction scores for each cashier.
+  '''
+  if len(obstructions) == 1:
+    return obstructions[0]
+  else:
+    avg = sum(obstructions)/len(obstructions)
+    obstructions.remove(max(obstructions))
+
+    sub = 0
+    for o in obstructions:
+      sub += sub_constant * o
+    
+    return avg - sub
 
 #########
 # POINT #
